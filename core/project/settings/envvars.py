55092a4e9d4ff77bd6e4041725f3f75db0dc5ede
Kevin Liu <kevin.liu4@mail.mcgill.ca>
2024-07-19 22:09:00 -0400

    Apply pre-commit fixes

--- core/project/settings/envvars.py
+++ core/project/settings/envvars.py
@@ -1,6 +1,5 @@
 from core.core.utils.collection import deep_update
 from core.core.utils.settings import get_settings_from_environment
-
 """
 This take env variable with matching prefix, strips it,
 and adds it to global settings.
@@ -11,7 +10,6 @@ export CORESETTINGS_IN_DOCKER=True
 will add IN_DOCKER = True to settings
 
 """
-
 # globals() is a dictionary that holds all the global variables
 # this should not break as it was processed from the initializer
 deep_update(globals(), get_settings_from_environment(ENVVAR_SETTINGS_PREFIX))  # type: ignore # noqa: F821
